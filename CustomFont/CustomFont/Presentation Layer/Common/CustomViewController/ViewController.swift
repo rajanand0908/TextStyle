0d671c341b01c63ee0cf95127d47251a1759dae0
Raj Anand <raj.anand0908@gmail.com>
2022-12-17 17:40:18 +0530

    [TASK] Implemented custom view controller

---
+++ CustomFont/CustomFont/Presentation Layer/Common/CustomViewController/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CustomFont
+//
+//  Created by Raj Anand on 17/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  // MARK: - Lifecycle
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
